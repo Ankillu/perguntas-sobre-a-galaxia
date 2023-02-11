1aa1408765dac9eb6ddceceb5f6dd5608d72002e
Angelo Facchin <120346508+Ankillu@users.noreply.github.com>
2023-02-11 16:03:18 -0300

    Create perguntasgalaxia.py

---
+++ perguntasgalaxia.py
@@ -0,0 +1,15 @@
+print("Olá a todos respondam as perguntas")
+resposta = input("Qual é mais denso? Uma nebulosa ou um buraco negro?")
+if resposta == ("buraco negro"):
+    print("Parabéns, você acertou")
+if resposta == ("Nebulosa"):
+    print("Você errou")
+resposta1 = input("É falso ou e verdade que Saturno boiaria na água")
+if resposta1 == ("Verdadeiro"):
+    print("Parabéns você acertou")
+if resposta1 == ("Falso"):
+    print("Você errou")
+if resposta1 == ("verdadeiro"):
+    print("Parabéns você acertou")
+if resposta1 == ("falso"):
+    print("Você errou")
